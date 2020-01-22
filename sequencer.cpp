82be9d38e244fa6e776233911a1ad66f6386f9fd
Jonathan Foucher <jfoucher@gmail.com>
2020-01-22 11:29:35 +0100

    Make use of interrupts to handle rotary encoders and switches

--- sequencer.cpp
+++ sequencer.cpp
@@ -3,6 +3,7 @@
 #include "instruments.h"
 #include "Adafruit_MCP23017.h"
 #include <Arduino.h>
+#include <EEPROM.h>
 
 #define DEBUG 1
 #define CHIP1ADDR 0x21
@@ -13,7 +14,8 @@
 #define LED_COLS 3
 
 #define ENCODER_TEMPO 0
-
+#define ENCODER_PITCH 1
+#define ENCODER_VOLUME 2
 
 Adafruit_MCP23017 mcp;
 Adafruit_MCP23017 mcp2;
@@ -22,10 +24,6 @@ Adafruit_MCP23017 mcp3;
 OPL2 opl2;
 
 typedef struct  {
-  int pin1;
-  int pin2;
-  int pin1_val;
-  int pin2_val;
   int value;
   int max_value;
   int min_value;
@@ -33,94 +31,116 @@ typedef struct  {
 } Encoder;
 
 
+
+
 Encoder encoders[] = {
-  {8, 9, 0, 0, 100, 300, 20, 10}
+  // Tempo
+  {100, 600, 20, 10},
+  // Number of steps
+  {1, 32, 0, 1},
+  // Pitch
+  {0, 1023, 0, 1},
+  // Voice select
+  {0, 10, 0, 1},
+  // Decay
+  {0, 15, 0, 1},
+  // Sustain
+  {0, 15, 0, 1},
+  // Release
+  {0, 15, 0, 1},
+  
+  // Volume
+  {0, 15, 0, 1},
+  // Waveform
+  {0, 2, 0, 1},
+  // Feedback
+  {0, 2, 0, 1},
+  // Voice select
+  {0, 11, 0, 1},
+  // Step select
+  {0, 31, 0, 1},
+  // Amplitude Modulation
+  {0, 2, 0, 1},
+  // Vibrato
+  {0, 2, 0, 1},
 };
 
-int encoder0PinALast = LOW;
-int encoder1PinALast = LOW;
-int encoder2PinALast = LOW;
-int encoder3PinALast = LOW;
-int encoder4PinALast = LOW;
-int encoder5PinALast = LOW;
-int n = LOW;
-int n1 = LOW;
-int n2 = LOW;
-int n3 = LOW;
-int n4 = LOW;
-int n5 = LOW;
-
 int i = 0;
-volatile bool drumsOn = true;
-volatile bool changeModulator = false;
-int note = 0;
-int octave = 0;
+
+int note = 1;
+int octave = 1;
 int tempo = 100;
-
-bool attackType = false;
-
-int attackCarrier = 3;
-int attackModulator = 3;
-
-int decayCarrier = 3;
-int decayModulator = 3;
-
-int sustainCarrier = 3;
-int sustainModulator = 3;
-
-int releaseCarrier = 3;
-int releaseModulator = 3;
-
-int noteCarrier = 30;
-int noteModulator = 1;
+int nSteps = 32;
+bool tempoEncoder = true;
 
 int selectedChannel = 0;
 
-int modulatorWaveform = 0;
-int carrierWaveform = 0;
+// Which note is selected.
+int selectedStep = 0;
+
 
 volatile boolean awakenByInterrupt = false;
+volatile boolean encoderInterrupt = false;
+volatile boolean switchInterrupt = false;
 
 int leds[LED_ROWS * LED_COLS] = {false};
 
-//Data for 6 voices, Carrier ADSR, carrier octave, carrier note, carrier wave form, carrier volume, modulator ADSR, modulator multiplier
-int noteData[6][11] = {
-  // A  D  S  R  WF Vo  MA MD MS MR
-    {0, 0, 0, 0, 0, 15, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 15, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 15, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 15, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 15, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 15, 0, 0, 0, 0},
+typedef struct  {
+  uint8_t attack;
+  uint8_t decay;
+  uint8_t sustain;
+  uint8_t release;
+  uint8_t waveform;
+  uint8_t volume;
+  uint8_t feedback;
+  uint8_t vibrato;
+  uint8_t tremolo;
+  uint8_t m_attack;
+  uint8_t m_decay;
+  uint8_t m_sustain;
+  uint8_t m_release;
+  uint8_t m_volume;
+  uint8_t m_waveform;
+  uint8_t m_mult;
+} Channel;
+
+
+Channel channels[6] = {
+    {14, 6, 6, 9, 0, 15, 1, 1, 1, 10, 2, 6, 9, 15, 3, 1},
+    {14, 6, 6, 9, 0, 15, 1, 1, 1, 10, 2, 6, 9, 15, 3, 1},
+    {14, 6, 6, 9, 0, 15, 1, 1, 1, 10, 2, 6, 9, 15, 3, 1},
+    {14, 6, 6, 9, 0, 15, 1, 1, 1, 10, 2, 6, 9, 15, 3, 1},
+    {14, 6, 6, 9, 0, 15, 1, 1, 1, 10, 2, 6, 9, 15, 3, 1},
+    {14, 6, 6, 9, 0, 15, 1, 1, 1, 10, 2, 6, 9, 15, 3, 1},
 };
 
-int tones[6][8][3] = {
-  {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
-  {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
-  {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
-  {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
-  {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
-  {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
-};
-
-// Array of booleans.
+// Array of ints.
 // 32 for each voice.
 // First 6 are melodic voices, last 5 are percussion voices
-bool playNotes[11][32];
+// For melodic voices, the value is the pitch of that note.
+// For percussion voices a value other than 0 means play.
+int playNotes[11][32];
+
+int pinInt;
+int switchInt;
+
+void pinChanged() {
+  encoderInterrupt = true;
+}
+
+void switchChanged() {
+  switchInterrupt = true;
+}
+
+ISR(TIMER1_COMPA_vect){//timer1 interrupt plays music
+  awakenByInterrupt = true;
+}
 
 void setup() {
   Serial.begin(9600);
-  mcp.begin();
 
-  for(int i = 0; i <= 15;i++) {
-    mcp.pinMode(i, INPUT);
-    mcp.pullUp(i, HIGH);
-  }
-  
-  
   mcp2.begin(1);
   mcp2.pinMode(8, OUTPUT);
-  mcp2.pinMode(0, OUTPUT);
 
   mcp3.begin(2);
   mcp3.pinMode(0, OUTPUT);
@@ -169,15 +189,48 @@ void setup() {
   TCCR1B |= (1 << CS12) | (1 << CS10);  
   // enable timer compare interrupt
   TIMSK1 |= (1 << OCIE1A);
+
+  for (int m = 0; m < 32; m++) {
+    playNotes[selectedChannel][m] = m % 2 == 0 ? 32 : 0;
+    playNotes[1][m] = 40;
+    playNotes[7][m] = m % 4 == 0 ? 1 : 0;
+    playNotes[8][m] = (m + 1) % 4 == 0 ? 1 : 0;
+  }
+
+  pinMode(3, INPUT);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(2, INPUT);
+  pinMode(2, INPUT_PULLUP);
+
+  mcp.begin();
+
+  mcp.setupInterrupts(false, false, LOW);
   
-  //attachInterrupt(digitalPinToInterrupt(2), toggleDrums, RISING);
+  for(int i = 0; i <= 15;i++) {
+    mcp.pinMode(i, INPUT);
+    mcp.pullUp(i, HIGH);
+  }
+
+  for(int i = 0; i <= 7;i++) {
+    mcp.setupInterruptPin(i, CHANGE); 
+  }
+
+  mcp2.setupInterrupts(false, false, LOW);
+  mcp2.pinMode(0, INPUT);
+  mcp2.pullUp(0, HIGH);
+  mcp2.setupInterruptPin(0, CHANGE); 
+
+  pinInt = digitalPinToInterrupt(3);
+  switchInt = digitalPinToInterrupt(2);
+  attachInterrupt(pinInt, pinChanged, FALLING);
+  attachInterrupt(switchInt, switchChanged, FALLING);
+  pinMode(3, INPUT);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(2, INPUT);
+  pinMode(2, INPUT_PULLUP);
 }
 
 
-
-ISR(TIMER1_COMPA_vect){//timer1 interrupt plays music
-  awakenByInterrupt = true;
-}
 void play();
 
 void play() {
@@ -185,282 +238,33 @@ void play() {
     mcp2.digitalWrite(8, HIGH);
   }
 
-  OCR1A = (int) (16000000 / (tempo * 8 * 4)) - 1;
-  
-  int rel = 3;
-
-  //Serial.println(rel);
-  int noteHeight = 500; //analogRead(A5);
-  //int type = digitalRead(7);
-
   // play melodic instruments if required
   for(int j = 0; j<= 6; j++) {
-    // This is wrong, all instruments should play, not only the selected one.
-    if (noteData[j][10] == 1) {
-      //Add rotary dial to set feedback
-      opl2.setFeedback(j, 0);
-
-      //Can't remember what this is for.
-      opl2.setSynthMode(j, false);
-
-      //Add button to set Tremolo/vibrato
-      //opl2.setTremolo   (j, CARRIER, true);
-      opl2.setVibrato   (j, CARRIER, true);
-
-      //Set carrier multiplier to one because otherwise low notes are impossible
-      opl2.setMultiplier(j, CARRIER, 0x01);
-
-      // This is the way to adjust the modulator frequency. There is a button for that.
-      opl2.setMultiplier(j, MODULATOR, noteModulator);
-      // Add dial to set waveform
-      opl2.setWaveForm(j, MODULATOR, modulatorWaveform);
-
-      //Add button to set volume per channel
-      opl2.setVolume(j, MODULATOR, 0);
-      opl2.setAttack    (j, MODULATOR, attackModulator);
-      opl2.setDecay     (j, MODULATOR, decayModulator);
-      opl2.setSustain   (j, MODULATOR, sustainModulator);
-      opl2.setRelease   (j, MODULATOR, releaseModulator);
-      
-      octave = noteCarrier >> 4;
-      note = (int)((float)((noteCarrier % 16) & 0xf) / 16.0 * 12.0);
-
-      //Add button to set volume per channel
-      opl2.setVolume(j, CARRIER, 0);
-      opl2.setWaveForm(j, CARRIER, carrierWaveform);
-      opl2.setAttack    (j, CARRIER, attackCarrier);
-      opl2.setDecay     (j, CARRIER, decayCarrier);
-      opl2.setSustain   (j, CARRIER, sustainCarrier);
-      opl2.setRelease   (j, CARRIER, releaseCarrier);
-      
-    }
-    if (playNotes[j][i%8]) {
+    if (playNotes[j][i]) {
+      octave = playNotes[j][i] >> 4;
+      note = (int)((float)(playNotes[j][i] & 0xf) / 16.0 * 12.0);
       opl2.playNote(j, octave, note);
     }
   }
 
   // play drums instruments if required
-  opl2.setDrums(playNotes[6][i % 32], playNotes[7][i % 32], playNotes[8][i % 32], playNotes[9][i % 32], playNotes[10][i % 32]);
+  opl2.setDrums(playNotes[6][i], playNotes[7][i], playNotes[8][i], playNotes[9][i], playNotes[10][i]);
 
-  //Flash tempo led.
+  // Flash tempo led.
   // Not sure if that will be necessary now.
   if ((i + 3) % 4 == 0) {
     mcp2.digitalWrite(8, LOW);
   }
   i++;
-  if (i >= 32) {
+  if (i >= nSteps) {
     i = 0;
   }
   awakenByInterrupt = false;
 }
 
-int readEncoder(int encoder_id);
 
-int readEncoder(int encoder_id) {
-  Encoder encoder = encoders[encoder_id];
-  int n = mcp.digitalRead(encoder.pin1);
-  
-  if ((encoder.pin1_val == LOW) && (n == HIGH)) {
-    if (mcp.digitalRead(encoder.pin2) == LOW) {
-      encoder.value -= encoder.step;
-      if (encoder.value < encoder.min_value){
-        encoder.value = encoder.min_value;
-      }
-    } else {
-      encoder.value += encoder.step;
-      if (encoder.value > encoder.max_value){
-        encoder.value = encoder.max_value;
-      }
-    }
-  }
-  encoders[encoder_id].pin1_val = n;
-  Serial.println(encoder.value);
-  encoders[encoder_id].value = encoder.value;
-  return encoder.value;
-}
-void readTempo();
-
-void readTempo() {
-  tempo = readEncoder(ENCODER_TEMPO);
-}
-
-void readAttack();
-void readAttack() {
-  n1 = mcp.digitalRead(10);
-  if ((encoder1PinALast == LOW) && (n1 == HIGH)) {
-    if (mcp.digitalRead(11) == LOW) {
-      if (attackType) {
-        attackCarrier+=1;
-        if (attackCarrier >= 15) {
-          attackCarrier = 15;
-        }
-      } else {
-        attackModulator+=1;
-        if (attackModulator >= 15) {
-          attackModulator = 15;
-        }
-      }
-    } else {
-      if (attackType) {
-        attackCarrier--;
-        if (attackCarrier <= 0) {
-          attackCarrier = 0;
-        }
-      } else {
-        attackModulator--;
-        if (attackModulator <= 0) {
-          attackModulator = 0;
-        }
-      }
-    }
-  }
-  encoder1PinALast = n1;
-}
-
-void readDecay();
-void readDecay() {
-  n3 = mcp.digitalRead(7);
-  if ((encoder3PinALast == LOW) && (n3 == HIGH)) {
-    if (mcp.digitalRead(6) == LOW) {
-      if (attackType) {
-        decayCarrier++;
-        if (decayCarrier >= 15) {
-          decayCarrier = 15;
-        }
-      } else {
-        decayModulator++;
-        if (decayModulator >= 15) {
-          decayModulator = 15;
-        }
-      }
-    } else {
-      if (attackType) {
-        decayCarrier--;
-        if (decayCarrier <= 0) {
-          decayCarrier = 0;
-        }
-      } else {
-        decayModulator--;
-        if (decayModulator <= 0) {
-          decayModulator = 0;
-        }
-      }
-    }
-  }
-  encoder3PinALast = n3;
-}
-
-
-void readRelease();
-void readRelease() {
-  n5 = mcp.digitalRead(2);
-  if ((encoder5PinALast == LOW) && (n5 == HIGH)) {
-    if (mcp.digitalRead(3) == LOW) {
-      if (attackType) {
-        releaseCarrier++;
-        if (releaseCarrier >= 15) {
-          releaseCarrier = 15;
-        }
-      } else {
-        releaseModulator++;
-        if (releaseModulator >= 15) {
-          releaseModulator = 15;
-        }
-      }
-    } else {
-      if (attackType) {
-        releaseCarrier--;
-        if (releaseCarrier <= 0) {
-          releaseCarrier = 0;
-        }
-      } else {
-        releaseModulator--;
-        if (releaseModulator <= 0) {
-          releaseModulator = 0;
-        }
-      }
-    }
-  }
-  encoder5PinALast = n5;
-}
-void readSustain();
-void readSustain() {
-  n4 = mcp.digitalRead(5);
-  if ((encoder4PinALast == LOW) && (n4 == HIGH)) {
-    if (mcp.digitalRead(4) == LOW) {
-      if (attackType) {
-        sustainCarrier++;
-        if (sustainCarrier >= 15) {
-          sustainCarrier = 15;
-        }
-      } else {
-        sustainModulator++;
-        if (sustainModulator >= 15) {
-          sustainModulator = 15;
-        }
-      }
-    } else {
-      if (attackType) {
-        sustainCarrier--;
-        if (sustainCarrier <= 0) {
-          sustainCarrier = 0;
-        }
-      } else {
-        sustainModulator--;
-        if (sustainModulator <= 0) {
-          sustainModulator = 0;
-        }
-      }
-    }
-  }
-  encoder4PinALast = n4;
-}
-
-void readNote();
-void readNote() {
-  n2 = mcp.digitalRead(14);
-  if ((encoder2PinALast == LOW) && (n2 == HIGH)) {
-    if (mcp.digitalRead(13) == LOW) {
-      if (attackType) {
-        noteCarrier+=1;
-        if (noteCarrier >= 127) {
-          noteCarrier = 127;
-        }
-      } else {
-        noteModulator+=1;
-        if (noteModulator >= 7) {
-          noteModulator = 7;
-        }
-      }
-    } else {
-      if (attackType) {
-        noteCarrier--;
-        if (noteCarrier <= 0) {
-          noteCarrier = 0;
-        }
-      } else {
-        noteModulator--;
-        if (noteModulator <= 0) {
-          noteModulator = 0;
-        }
-      }
-    }
-  }
-  encoder2PinALast = n2;
-}
-
-int debounceDelay = 300;
-unsigned int lastDebounceTime = millis();
-unsigned int lastSwitchDebounceTime = millis();
-
-int oldSwitchPos = HIGH;
-int oldSwitch0Pos = HIGH;
-
-int oldVal = 0;
 int currentRow = 0;
-int litLed = 0;
 
-unsigned int lastChanged = millis();
 int changeLedRow();
 int changeLedRow() {
   // Switch rows
@@ -469,7 +273,6 @@ int changeLedRow() {
     if (k != currentRow) {
       mcp3.digitalWrite(k, HIGH);
     }
-    
   }
   
   for (int j = 0; j < LED_COLS;j++) {
@@ -482,73 +285,156 @@ int changeLedRow() {
     currentRow = 0;
   }
 }
+
 int ledOn(int ref, int level);
 int ledOn (int ref, int level) {
   leds[ref] = true;
 }
-int ledOff (int ref, int level);
-int ledOff (int ref, int level) {
+int ledOff (int ref);
+int ledOff (int ref) {
   leds[ref] = false;
+}
+
+void saveChannel(int j);
+void saveChannel(int j) {
+  opl2.setFeedback(j, channels[j].feedback);
+
+  //Can't remember what this is for.
+  opl2.setSynthMode(j, false);
+
+  //Add button to set Tremolo/vibrato
+  opl2.setTremolo   (j, CARRIER, channels[j].tremolo > 0);
+  opl2.setVibrato   (j, CARRIER, channels[j].vibrato > 0);
+
+  //Set carrier multiplier to one because otherwise low notes are impossible
+  opl2.setMultiplier(j, CARRIER, 0x01);
+  
+  // This is the way to adjust the modulator frequency. There is a button for that.
+  opl2.setMultiplier(j, MODULATOR, channels[j].m_mult);
+  // Add dial to set waveform
+  opl2.setWaveForm(j, MODULATOR, channels[j].m_waveform);
+
+  //Add button to set volume per channel
+  opl2.setVolume(j, MODULATOR, 15 - channels[j].m_volume);
+  opl2.setAttack    (j, MODULATOR, channels[j].m_attack);
+  opl2.setDecay     (j, MODULATOR, channels[j].m_decay);
+  opl2.setSustain   (j, MODULATOR, channels[j].m_sustain);
+  opl2.setRelease   (j, MODULATOR, channels[j].m_release);
+  
+  //Add button to set volume per channel
+  opl2.setVolume(j, CARRIER, 15 - channels[j].volume);
+  opl2.setWaveForm(j, CARRIER, channels[j].waveform);
+  opl2.setAttack    (j, CARRIER, channels[j].attack);
+  opl2.setDecay     (j, CARRIER, channels[j].decay);
+  opl2.setSustain   (j, CARRIER, channels[j].sustain);
+  opl2.setRelease   (j, CARRIER, channels[j].release);
 }
 
 void loop() {
   if (awakenByInterrupt) {
+    saveChannel(selectedChannel);
+    saveChannel(1);
     play();
   }
-  readNote();
-  readTempo();
-  readAttack();
-  readDecay();
-  readSustain();
-  readRelease();
 
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    int reading = mcp.digitalRead(0);
-    if (reading != oldSwitch0Pos) {
-      oldSwitch0Pos = reading;
-      lastDebounceTime = millis();
-      if (reading == LOW) {
-        
-        attackType = !attackType;
-        mcp2.digitalWrite(0, attackType ? HIGH : LOW);
-        Serial.print(!attackType ? "modulator" : "carrier");
-        Serial.println();
+  if (encoderInterrupt) {
+    //An encoder was turned
+    int pin = mcp.getLastInterruptPin();
+    int val = mcp.getLastInterruptPinValue();
+    mcp.readGPIO(0);
+    if (MCP23017_INT_ERR == val || MCP23017_INT_ERR == pin || val == 1) {
+      encoderInterrupt = false;
+      
+      return ;
+    }
+    
+    int other = mcp.digitalRead(pin+8);
+
+    Encoder encoder = encoders[0];
+    if (pin == ENCODER_PITCH) {
+      encoder = encoders[2];
+      encoder.value = playNotes[selectedChannel][selectedStep];
+    } else if (pin == ENCODER_VOLUME) {
+      encoder = encoders[7];
+      encoder.value = channels[selectedChannel].volume;
+    } else if (pin == ENCODER_TEMPO) {
+      if (tempoEncoder == true) {
+        encoder = encoders[0];
+        encoder.value = tempo;
+      } else {
+        encoder = encoders[1];
+        encoder.value = nSteps;
       }
     }
+
+    if (other == 0) {
+      encoder.value -= encoder.step;
+      if (encoder.value < encoder.min_value){
+        encoder.value = encoder.min_value;
+      }
+    } else {
+      encoder.value += encoder.step;
+      if (encoder.value > encoder.max_value){
+        encoder.value = encoder.max_value;
+      }
+    }
+
+    if (pin == ENCODER_PITCH) {
+      playNotes[selectedChannel][selectedStep] = encoder.value;
+      Serial.print("pitch for  step ");
+      Serial.print(selectedStep);
+      Serial.print(" of channel ");
+      Serial.print(selectedChannel);
+      Serial.print(": ");
+      Serial.println(playNotes[selectedChannel][selectedStep]);
+    } else if (pin == ENCODER_VOLUME) {
+      channels[selectedChannel].volume = encoder.value;
+      Serial.print("Volume: ");
+      Serial.println(channels[selectedChannel].volume);
+    } else if (pin == ENCODER_TEMPO) {
+      if (tempoEncoder == true) {
+        tempo = encoder.value;
+        OCR1A = (int) (16000000 / (tempo * 8 * 4)) - 1;
+        Serial.println(tempo);
+      } else {
+        nSteps = encoder.value;
+        // Serial.print("num steps ");
+        // Serial.println(nSteps);
+      }
+      //Serial.println(tempo);
+    }
+    mcp.readGPIO(0);
+    //while( ! (mcp.readGPIO(0) ));
+
+    encoderInterrupt = false;
   }
-  
+
+  if (switchInterrupt) {
+    // a switch was pressed
+    int pin = mcp2.getLastInterruptPin();
+    int val = mcp2.getLastInterruptPinValue();
+    mcp2.readGPIO(0);
+    if (MCP23017_INT_ERR == val || MCP23017_INT_ERR == pin || val == 1) {
+      switchInterrupt = false;
+      return ;
+    }
+
+    if (pin == 0) {
+      tempoEncoder = !tempoEncoder;
+      if (tempoEncoder) {
+        ledOn(1,1);
+      } else {
+        ledOff(1);
+      }
+    }
+    
+    Serial.print("switch interrupt ");
+    Serial.println(pin);
+
+    switchInterrupt = false;
+  }
 
   changeLedRow();
-
-  
-  if ((millis() - lastSwitchDebounceTime) > debounceDelay) {
-    int waveform = digitalRead(7);
-    if (waveform != oldSwitchPos) {
-      lastSwitchDebounceTime = millis();
-      oldSwitchPos = waveform;
-      if (waveform == LOW) {
-        if (attackType) {
-          carrierWaveform++;
-          if (carrierWaveform > 3) {
-            carrierWaveform = 0;
-          }
-          Serial.print("carrierWavefrom: ");
-          Serial.println(carrierWaveform);
-
-    
-        } else {
-          modulatorWaveform++;
-          if (modulatorWaveform > 3) {
-            modulatorWaveform = 0;
-          }
-          Serial.print("modulatorWaveform: ");
-          Serial.println(modulatorWaveform);
-
-        }
-      }
-      
-    }
-  }
-  
-  
 }
+
+
